482bde0ccb02018762066de677b4acae2d70c5fc
Faizuddin Mohammed <fmohammed@nextdoor.com>
2022-07-27 16:42:23 -0400

    Delegation Pattern - initial setiup

---
+++ Rabble Wabble/Controllers/SelectQuestionGroupViewControler.swift
@@ -0,0 +1,51 @@
+//
+//  SelectQuestionGroupViewControler.swift
+//  Rabble Wabble
+//
+//  Created by Mohammed Faizuddin on 7/20/22.
+//
+
+import UIKit
+
+public class SelectQuestionViewController: UIViewController {
+
+    @IBOutlet internal var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+
+    public let questionsGroup = QuestionGroup.allGroups()
+    private var selectedQuestionGroup: QuestionGroup!
+}
+
+extension SelectQuestionViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionsGroup.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionGroupCell") as! QuestionGroupCell
+        let questionGroup = questionsGroup[indexPath.row]
+        cell.titleLabel.text = questionGroup.title
+        return cell
+    }
+}
+
+extension SelectQuestionViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        selectedQuestionGroup = questionsGroup[indexPath.row]
+        return indexPath
+    }
+
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let viewController = segue.destination as? QuestionViewController else { return }
+        viewController.questionGroup = selectedQuestionGroup
+    }
+}
---
+++ Rabble Wabble/Views/QuestionGroupCell.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroupCell.swift
+//  Rabble Wabble
+//
+//  Created by Mohammed Faizuddin on 7/20/22.
+//
+
+import UIKit
+
+public class QuestionGroupCell: UITableViewCell {
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var percentageLabel: UILabel!
+}
