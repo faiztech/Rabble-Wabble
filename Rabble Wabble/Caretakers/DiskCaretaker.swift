baa7957d3e41ddd61a78a08b90f127730b5234ae
faiztech <mohammedfaizuddin@live.com>
2022-09-04 16:25:41 -0400

    Adds setup for Memento Pattern

---
+++ Rabble Wabble/Caretakers/DiskCaretaker.swift
@@ -0,0 +1,45 @@
+//
+//  DiskCaretaker.swift
+//  Rabble Wabble
+//
+//  Created by Mohammed Faizuddin on 9/4/22.
+//
+
+import Foundation
+
+public final class DiskCaretaker {
+    public static let decoder = JSONDecoder()
+    public static let encoder = JSONEncoder()
+
+    public static func save<T: Codable>(_ object: T, to fileName: String) throws {
+        do {
+            let url = createDocumentURL(withFileName: fileName)
+            let data = try encoder.encode(object)
+            try data.write(to: url, options: .atomic)
+        } catch (let error) {
+            print("Save Failed: Object: \(object) Error \(error)")
+            throw error
+        }
+    }
+
+    public static func retrieve<T: Codable>(_ type: T.Type, from fileName: String) throws -> T {
+        let url = createDocumentURL(withFileName: fileName)
+        return try retrieve(T.self, from: url)
+    }
+
+    public static func retrieve<T: Codable>(_ type: T.Type, from url: URL) throws -> T {
+        do {
+            let data = try Data(contentsOf: url)
+            return try decoder.decode(T.self, from: data)
+        } catch(let error) {
+            print("Retrieve Failed: URL: \(url) Error \(error)")
+            throw error
+        }
+    }
+
+    public static func createDocumentURL(withFileName fileName: String) -> URL {
+        let fileManager = FileManager.default
+        let url = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return url.appendingPathComponent(fileName).appendingPathComponent("json")
+    }
+}
--- Rabble Wabble/Models/Models.swift
+++ Rabble Wabble/Models/Models.swift
@@ -7,14 +7,34 @@
 
 import Foundation
 
-public struct Question {
+public class Question: Codable {
     public let answer: String
     public let hint: String?
     public let prompt: String
+
+    public init(answer: String, hint: String?, prompt: String) {
+        self.answer = answer
+        self.hint = hint
+        self.prompt = prompt
+    }
 }
 
-public struct QuestionGroup {
+public class QuestionGroup: Codable {
+    public class Score: Codable {
+        public var correctCount: Int = 0
+        public var incorrectCount: Int = 0
+        public init() { }
+    }
     public let questions: [Question]
+    public var score: Score
     public let title: String
+
+    public init(questions: [Question],
+         score: Score = Score(),
+         title: String) {
+        self.questions = questions
+        self.score = score
+        self.title = title
+    }
 }
 
